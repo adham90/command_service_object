f832df76fc4a7d9ae8558f384d85bc808ee173ed
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-04-10 20:04:36 +0200

    add skip command and error options

--- lib/generators/service/service_generator.rb
+++ lib/generators/service/service_generator.rb
@@ -8,12 +8,20 @@ class ServiceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   argument :usecases, type: :array, default: [], banner: 'usecase usecase'
+  class_option :skip_command, type: :boolean, default: false,
+                              desc: 'skip command file generation'
+  class_option :skip_error, type: :boolean, default: false,
+                            desc: 'skip error file generation'
 
   def create_base_dir
-    directory 'services', 'app/services' unless File.exist?('app/services')
+    return if File.exist?('app/services')
+
+    directory 'services', 'app/services'
   end
 
   def create_service_dir
+    return if File.exist?("app/services/#{service_name}")
+
     empty_directory("app/services/#{service_name}")
     empty_directory("app/services/#{service_name}/usecases")
     empty_directory("app/services/#{service_name}/commands")
@@ -28,8 +36,8 @@ class ServiceGenerator < Rails::Generators::NamedBase
       e_path = "app/services/#{service_name}/errors/#{usecase.underscore}.rb"
 
       template 'usecase.rb.erb', u_path
-      template 'command.rb.erb', c_path
-      template 'error.rb.erb',   e_path
+      template 'command.rb.erb', c_path unless options.skip_command?
+      template 'error.rb.erb',   e_path unless options.skip_error?
     end
   end
 
