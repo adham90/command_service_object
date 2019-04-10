4a854901766009c9798050d5ccb2952e572c7143
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-04-10 02:17:29 +0200

    init commit

---
+++ lib/command_service_object.rb
@@ -0,0 +1,6 @@
+require "command_service_object/version"
+
+module CommandServiceObject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/command_service_object/version.rb
@@ -0,0 +1,3 @@
+module CommandServiceObject
+  VERSION = "0.1.0"
+end
