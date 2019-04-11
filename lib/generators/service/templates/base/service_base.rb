cb661a378314571efdbb23fe0fbed5b7b486c8a6
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-04-11 12:50:34 +0200

    add rollback and error logging

--- lib/generators/service/templates/base/service_base.rb
+++ lib/generators/service/templates/base/service_base.rb
@@ -10,6 +10,28 @@ class ServiceBase
   def self.call(command)
     raise Errors::InvalidCommand, command.class if command.invalid?
 
-    new(command).call
+    obj = new(command)
+    obj.call
+  rescue StandardError => e
+    error(e)
+    obj.send(:rollback)
+    raise e
+  end
+
+  private
+
+  def self.error(err)
+    # don't log custom errors if you want :D
+    return if err.class.name.start_with?(self.class.name.split('::').first)
+
+    #
+    # Add your logging logic
+    # ex:
+    #   Rollbar.error(err)
+    #
+  end
+
+  def rollback
+    raise NotImplementedError
   end
 end
