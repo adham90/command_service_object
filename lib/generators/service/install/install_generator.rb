dfc0fdbff1abe26c30e43255190d618d8b1398ed
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-04-14 23:43:17 +0200

    fix InstallGenerator base dir

--- lib/generators/service/install/install_generator.rb
+++ lib/generators/service/install/install_generator.rb
@@ -3,8 +3,10 @@ require 'rails/generators'
 module Service
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path('.', __dir__)
+
       def create_base_dir
-        directory 'base', 'app/services'
+        directory 'templates', 'app/services'
       end
     end
   end
