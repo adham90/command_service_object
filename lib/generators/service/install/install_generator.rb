444c0690275ab93ac199c310a5d04ccb1298456b
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-04-15 00:35:33 +0200

    move initializer generation with install generator

--- lib/generators/service/install/install_generator.rb
+++ lib/generators/service/install/install_generator.rb
@@ -3,10 +3,20 @@ require 'rails/generators'
 module Service
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path('.', __dir__)
+      source_root File.expand_path('templates', __dir__)
 
-      def create_base_dir
-        directory 'templates', 'app/services'
+      def create_initializer_file
+        template 'initializer.rb', config_file_path
+      end
+
+      def create_services_dir
+        directory 'services', 'app/services'
+      end
+
+      private
+
+      def config_file_path
+        'config/initializers/command_service_object.rb'
       end
     end
   end
