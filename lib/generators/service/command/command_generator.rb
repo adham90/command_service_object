3fb048880cd8b7b6c592458358ae971d0e71e4c4
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-07-05 19:41:10 +0200

    remove model helper

--- lib/generators/service/command/command_generator.rb
+++ lib/generators/service/command/command_generator.rb
@@ -3,15 +3,13 @@ require_relative '../setup/setup_generator.rb'
 module Service
   module Generators
     class CommandGenerator < Rails::Generators::NamedBase
-      include CommandServiceObject::ModelHelper
-
       source_root File.expand_path('templates', __dir__)
 
       argument :commands, type: :array, default: [], banner: 'command command'
 
       def call
         invoke Service::Generators::SetupGenerator, [name]
-        @model_attributes = model_attributes
+        # @model_attributes = model_attributes
         commands.each do |c|
           @command = c.classify
           path = "app/services/#{service_name}/commands/#{c.underscore}.rb"
