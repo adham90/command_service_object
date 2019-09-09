9121fdfefa8b8375793c11904fdc4d81d15f4dee
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-09-09 22:19:16 +0200

    add subdir for test

--- lib/generators/service/test/test_generator.rb
+++ lib/generators/service/test/test_generator.rb
@@ -23,27 +23,43 @@ module Service
 
         tests.each do |t|
           @test = t.classify
-          path  = "#{dir}/#{t.underscore}_test.rb"
-          template 'minitest.rb.erb', path
+          path  = "#{dir}/usecases/#{t.underscore}_test.rb"
+          template 'minitest/usecase.rb.erb', path
         end
       end
 
       def rspec_test
         dir = create_test_dir('spec')
+        create_usecase_test(dir)
+        create_command_test(dir)
+      end
 
+      def create_usecase_test(dir)
         tests.each do |t|
           @test = t.classify
-          path  = "#{dir}/#{t.underscore}_spec.rb"
-          template 'rspec.rb.erb', path
+          path  = "#{dir}/usecases/#{t.underscore}_spec.rb"
+          template 'rspec/usecase.rb.erb', path
+        end
+      end
+
+      def create_command_test(dir)
+        tests.each do |t|
+          @test = t.classify
+          path  = "#{dir}/commands/#{t.underscore}_spec.rb"
+          template 'rspec/command.rb.erb', path
         end
       end
 
       def create_test_dir(path)
         test_path    = "#{path}/services"
         service_test = "#{test_path}/#{service_name}"
+        usecases_path = "#{test_path}/#{service_name}/usecases"
+        commands_path = "#{test_path}/#{service_name}/commands"
 
         empty_directory(test_path)    unless File.exist?(test_path)
         empty_directory(service_test) unless File.exist?(service_test)
+        empty_directory(usecases_path) unless File.exist?(usecases_path)
+        empty_directory(commands_path) unless File.exist?(commands_path)
 
         service_test
       end
