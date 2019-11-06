11e032e90c4ae3c3849635f2c9ad18aeef37aa1a
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2019-11-06 14:39:11 +0200

    edit command and case base

--- lib/generators/service/install/templates/services/case_base.rb
+++ lib/generators/service/install/templates/services/case_base.rb
@@ -2,13 +2,22 @@
 
 class CaseBase
   include CommandServiceObject::FailureHelper
-  include CommandServiceObject::Hooks
 
-  attr_reader :cmd
+  attr_reader :cmd, :issuer, :right_name
   alias_attribute :payload, :cmd
 
   def initialize(cmd)
     @cmd = cmd
+    @issuer = cmd.try(:issuer)
+    @right_name = "#{service_name}.#{case_name}"
+  end
+
+  def case_name
+    self.class.name.split('::').last.downcase
+  end
+
+  def service_name
+    self.class.name.split('::').first.remove('Service').downcase
   end
 
   def allowed?
--- lib/generators/service/install/templates/services/command_base.rb
+++ lib/generators/service/install/templates/services/command_base.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class CommandBase
-  include Virtus.model
   include ActiveModel::Validations
+  include Virtus.model(nullify_blank: true)
 
   # if you need to enable policies add issuer attribute
-  # attribute :issuer, User
-  # validates :issuer, presence: true
+  attribute :issuer, Object, default: :default_issuer
+
+  private
+
+  def default_issuer
+    nil
+  end
 end
