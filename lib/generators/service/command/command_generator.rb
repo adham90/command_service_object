0b3b66973137895cdf9b4ffaccda4e7c1152c96c
Adham EL-Deeb <adham.eldeeb90@gmail.com>
2020-09-10 12:23:19 +0200

    move test to generators dirs

--- lib/generators/service/command/command_generator.rb
+++ lib/generators/service/command/command_generator.rb
@@ -12,14 +12,25 @@ module Service
       def call
         invoke Service::Generators::SetupGenerator, [name]
         @model_attributes = model_attributes
+
         commands.each do |c|
           @command = c.classify
-          path = "app/services/#{service_name}/commands/#{c.underscore}.rb"
-          template 'command.rb.erb', path unless options.skip_command?
+          create_main(c)
+          create_test(c)
         end
       end
 
       private
+
+      def create_main(m)
+        path = "app/services/#{service_name}/commands/#{m.underscore}.rb"
+        template 'command.rb.erb', path unless options.skip_command?
+      end
+
+      def create_test(m)
+        path = "spec/services/#{service_name}/commands/#{m.underscore}_spec.rb"
+        template 'command_spec.rb.erb', path
+      end
 
       def service_name
         "#{name.underscore}_service"
--- lib/generators/service/external/external_generator.rb
+++ lib/generators/service/external/external_generator.rb
@@ -7,20 +7,27 @@ module Service
 
       argument :externals, type: :array, default: [], banner: 'external external'
 
-      def setup
+      def create_externals
         invoke Service::Generators::SetupGenerator, [name]
-      end
 
-      def create_micros
         externals.each do |m|
           @external = m.classify
-
-          path = "app/services/#{service_name}/externals/#{m.underscore}.rb"
-          template 'external.rb.erb', path
+          create_main(m)
+          create_test(m)
         end
       end
 
       private
+
+      def create_main(m)
+        path = "app/services/#{service_name}/externals/#{m.underscore}.rb"
+        template 'external.rb.erb', path
+      end
+
+      def create_test(m)
+        path = "spec/services/#{service_name}/externals/#{m.underscore}_spec.rb"
+        template 'external_spec.rb.erb', path
+      end
 
       def service_name
         "#{name.underscore}_service"
